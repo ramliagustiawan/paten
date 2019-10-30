f588842b42ef6c0ac96badacbd7af4270b38ad16
ramliagustiawan <ramliagustiawan@gmail.com>
2019-10-31 06:37:45 +0800

    menghilangkan front pd routeservice dan membuat authschafolding

--- routes/web.php
+++ routes/web.php
@@ -15,9 +15,14 @@
 //     return view('front.templates.homepage');
 // })->name('homepage');
 
+// Route::get('/', function () {
+//     return view('admin.templates.default');
+// })->name('default');
+
 Route::get('/', function () {
-    return view('admin.templates.default');
-})->name('default');
+    return view('welcome');
+});
+
 
 Route::get('/info/data', 'DataController@ijins')->name('info.data');
 Route::get('/info/non', 'DataController@nonijins')->name('info.non');
@@ -34,3 +39,7 @@ Route::resource('/iumk', 'IumkController');
 
 
 Route::resource('/report', 'ReportController');
+
+Auth::routes(['verify' => true]);
+
+Route::get('/home', 'HomeController@index')->name('home')->middleware('verified');
