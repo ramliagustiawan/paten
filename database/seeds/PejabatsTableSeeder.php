413e16da549b486e5c656934d7b83eaeac89e30e
ramliagustiawan <ramliagustiawan@gmail.com>
2019-12-10 13:29:16 +0800

    refresh paten

--- database/seeds/PejabatsTableSeeder.php
+++ database/seeds/PejabatsTableSeeder.php
@@ -24,5 +24,47 @@ class PejabatsTableSeeder extends Seeder
             'ketjabatan' => 'Sekretaris Kecamatan',
             'nip' => '19840813 200312 1 002',
         ]);
+
+        Pejabat::create([
+            'nama' => 'Hj. Harni Mahmud, S.AP',
+            'Jabatan' => 'a.n. Camat Dungingi',
+            'ketjabatan' => 'Kepala Seksi Pemerintahan',
+            'nip' => '19671121 200701 2 013',
+        ]);
+
+        Pejabat::create([
+            'nama' => 'Agumiaty Sulila, SP, M.Si',
+            'Jabatan' => 'a.n. Camat Dungingi',
+            'ketjabatan' => 'Kepala Seksi PMK',
+            'nip' => '19670805 200003 2 004',
+        ]);
+
+        Pejabat::create([
+            'nama' => 'Syamsudin Abdullah, S.IP',
+            'Jabatan' => 'a.n. Camat Dungingi',
+            'ketjabatan' => 'Kepala Seksi Ekbang',
+            'nip' => '19760213 200103 1 001',
+        ]);
+
+        Pejabat::create([
+            'nama' => 'Jasdy Biga, SE',
+            'Jabatan' => 'a.n. Camat Dungingi',
+            'ketjabatan' => 'Kepala Seksi Trantibum',
+            'nip' => '19660526 201001 1 001',
+        ]);
+
+        Pejabat::create([
+            'nama' => 'Jeaneth M. Djafar, SE',
+            'Jabatan' => 'a.n. Camat Dungingi',
+            'ketjabatan' => 'Kasubag KUK',
+            'nip' => '19770106 200604 2 015',
+        ]);
+
+        Pejabat::create([
+            'nama' => 'Marlin Bobihu, SE',
+            'Jabatan' => 'a.n. Camat Dungingi',
+            'ketjabatan' => 'Kasubag Program',
+            'nip' => '19750107 199402 2 001',
+        ]);
     }
 }
